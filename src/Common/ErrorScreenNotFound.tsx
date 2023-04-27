78724253674246f8e796ab6c2f1168b13cc08f62
Ash-exp <asutosh2000ad@gmail.com>
2023-04-27 14:34:41 +0530

    initial commit

--- src/Common/ErrorScreenNotFound.tsx
+++ src/Common/ErrorScreenNotFound.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import notFound from '../Assets/Img/ic-empty-error@2x.png'
-import { useHistory } from 'react-router'
+import { useHistory } from 'react-router-dom'
 import { ERROR_EMPTY_SCREEN } from './Constants'
 import GenericEmptyState from './EmptyState/GenericEmptyState'
 
 function ErrorScreenNotFound() {
-    const history = useHistory()
+    const {push} = useHistory()
     const redirectToHome = () => {
-        history.push('app/list')
+        push('/app/list')
     }
     const renderGenerateButton = () => {
         return (
