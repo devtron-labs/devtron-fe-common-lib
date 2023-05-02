560f0c02278e9e7ffca04efbfec0d372688c6b58
vivek-devtron <vivek@devtron.ai>
2023-05-02 14:22:58 +0530

    minor fix

--- src/Common/Api.ts
+++ src/Common/Api.ts
@@ -74,9 +74,9 @@ const responseMessages = {
 }
 
 function handleLogout() {
-    let cont = `${window.location.pathname.replace(process.env.PUBLIC_URL, '')}${window.location.search}`
+    const continueParam = `${window.location.pathname.replace(process.env.PUBLIC_URL, '')}${window.location.search}`
     const loginUrl = URLS.LOGIN_SSO
-    window.location.href = `${window.location.origin}${process.env.PUBLIC_URL}${loginUrl}?continue=${cont}`
+    window.location.href = `${window.location.origin}${process.env.PUBLIC_URL}${loginUrl}?continue=${continueParam}`
 }
 
 async function handleServerError(contentType, response) {
