2593aca22c79d12069375d434ac6919cf9a61807
shivani170 <shivani@devtron.ai>
2023-12-20 19:12:41 +0530

    fix for label type

--- src/Common/CustomInput/Types.ts
+++ src/Common/CustomInput/Types.ts
@@ -6,7 +6,7 @@ export interface CustomInputProps {
     onChange: (e: any) => void
     onFocus?: (e: any) => void
     autoComplete?: string
-    label?: string | (() => React.ReactNode)
+    label?: string |  React.ReactNode
     labelClassName?: string
     type?: HTMLInputTypeAttribute
     disabled?: boolean
