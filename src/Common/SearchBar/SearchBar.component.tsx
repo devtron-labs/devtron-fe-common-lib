a784138ebd0e6ef60baacce3732b86e378a6a060
AbhishekA1509 <abhishek@devtron.ai>
2024-01-20 13:27:13 +0530

    fix: search bar default classes and clear search on button press

--- src/Common/SearchBar/SearchBar.component.tsx
+++ src/Common/SearchBar/SearchBar.component.tsx
@@ -83,11 +83,12 @@ const SearchBar = ({
     const clearSearch = () => {
         inputRef.current.value = ''
         _applySearch('')
+        setShowClearButton(false)
     }
 
     return (
         <div className={containerClassName}>
-            <div className="search-bar bc-n50 dc__hover-border-n300 dc__block w-100 min-w-200 dc__position-rel en-2 bw-1 br-4 h-32">
+            <div className="search-bar bc-n50 focus-within-border-b5 dc__hover-border-n300 dc__block w-100 min-w-200 dc__position-rel en-2 bw-1 br-4 h-32">
                 <Search className="search-bar__icon dc__position-abs icon-color-n6 icon-dim-18" />
                 <input
                     placeholder="Search"
@@ -95,7 +96,7 @@ const SearchBar = ({
                     type="text"
                     {...inputProps}
                     defaultValue={initialSearchText}
-                    className={`search-bar__input bcn-0 dc__position-abs w-100 h-100 br-4 dc__no-border pt-6 pr-10 pb-6 pl-30 fs-13 lh-20 fw-4 cn-9 placeholder-cn5 ${
+                    className={`search-bar__input bc-n50 dc__position-abs w-100 h-100 br-4 dc__no-border pt-6 pr-10 pb-6 pl-30 fs-13 lh-20 fw-4 cn-9 placeholder-cn5 ${
                         showClearButton ? 'pr-30' : 'pr-10'
                     }`}
                     onChange={handleChange}
