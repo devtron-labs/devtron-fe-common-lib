82804583cd503e3bb0775500964cc2b76a57cf2b
Eshank Vaish <eshank@devtron.ai>
2024-01-25 13:43:13 +0530

    docs: add documentation for types

--- src/Common/SortableTableHeaderCell/types.ts
+++ src/Common/SortableTableHeaderCell/types.ts
@@ -1,9 +1,26 @@
 import { SortingOrder } from '../Constants'
 
 export interface SortableTableHeaderCellProps {
+    /**
+     * If true, the cell is sorted
+     */
     isSorted: boolean
+    /**
+     * Callback for handling the sorting of the cell
+     */
     triggerSorting: () => void
+    /**
+     * Current sort order
+     *
+     * Note: On click, the sort order should be updated as required
+     */
     sortOrder: SortingOrder
+    /**
+     * Label for the cell
+     */
     title: string
+    /**
+     * If true, the cell is disabled
+     */
     disabled: boolean
 }
