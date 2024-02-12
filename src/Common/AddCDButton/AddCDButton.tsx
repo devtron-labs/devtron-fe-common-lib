88f8498d6c20b6a7f8918706b644c4ea3bafaec6
shivani170 <shivani@devtron.ai>
2024-02-12 14:32:25 +0530

    fix for plus

--- src/Common/AddCDButton/AddCDButton.tsx
+++ src/Common/AddCDButton/AddCDButton.tsx
@@ -38,30 +38,28 @@ export default function AddCDButton({
                 className="default-tt"
                 arrow={false}
             >
-                <div>
-                    <svg
-                        x={referenceNode.x + (position === AddCDPositions.RIGHT ? -20 - 5 : referenceNode.width + 5)}
-                        // Here 10 is the height of the button / 2
-                        y={referenceNode.y + referenceNode.height / 2 - 10}
-                        width="20"
-                        height="20"
-                        viewBox="0 0 20 20"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                        data-testid={`add-cd-to-${position}`}
-                        onClick={handleAddCDClick}
-                        className="dc__outline-none-imp"
-                    >
-                        <rect width="20" height="20" rx="10" fill="#664BEE" className="add-cd-edge-btn" />
-                        <path
-                            d="M6.5 10H13.5M10 6.5V13.5"
-                            stroke="white"
-                            strokeWidth="1.5"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                        />
-                    </svg>
-                </div>
+                <svg
+                    x={referenceNode.x + (position === AddCDPositions.RIGHT ? -20 - 5 : referenceNode.width + 5)}
+                    // Here 10 is the height of the button / 2
+                    y={referenceNode.y + referenceNode.height / 2 - 10}
+                    width="20"
+                    height="20"
+                    viewBox="0 0 20 20"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                    data-testid={`add-cd-to-${position}`}
+                    onClick={handleAddCDClick}
+                    className="dc__outline-none-imp"
+                >
+                    <rect width="20" height="20" rx="10" fill="#664BEE" className="add-cd-edge-btn" />
+                    <path
+                        d="M6.5 10H13.5M10 6.5V13.5"
+                        stroke="white"
+                        strokeWidth="1.5"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                    />
+                </svg>
             </Tippy>
         )
     }
