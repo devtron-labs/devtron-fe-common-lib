a5ad36528cc50a1e6a3f2ece5897ac96fd0eb3db
AbhishekA1509 <abhishek@devtron.ai>
2024-02-19 21:09:41 +0530

    chore: remove unused constant

--- src/Shared/constants.tsx
+++ src/Shared/constants.tsx
@@ -13,12 +13,6 @@ export const STAGE_TYPE = {
     ROLLBACK: 'ROLLBACK',
 }
 
-export const MATERIAL_TYPE = {
-    ROLLBACK_MATERIAL_LIST: 'rollbackMaterialList',
-    INPUT_MATERIAL_LIST: 'inputMaterialList',
-    NONE: 'none',
-}
-
 export const SCAN_TOOL_ID_TRIVY = 3
 
 export const IMAGE_SCAN_TOOL = {
