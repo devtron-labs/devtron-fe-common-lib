a8134c3a06e47d7fecff78599cfac91d8a4689d6
Eshank Vaish <eshank@devtron.ai>
2024-02-28 11:56:23 +0530

    docs: add comments for GenericSectionErrorState types

--- src/Shared/Components/GenericSectionErrorState/types.ts
+++ src/Shared/Components/GenericSectionErrorState/types.ts
@@ -1,4 +1,12 @@
 export interface GenericSectionErrorStateProps {
+    /**
+     * Handler for reloading the section
+     */
     reload: () => void
+    /**
+     * If true, border is added to the section
+     *
+     * @default false
+     */
     withBorder?: boolean
 }
