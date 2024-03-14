53bc3996f8d51513329e8772ff54e88383589239
AbhishekA1509 <abhishek@devtron.ai>
2024-03-14 11:02:51 +0530

    feat: extend GenericErrorState

--- src/Shared/Components/GenericSectionErrorState/GenericSectionErrorState.component.tsx
+++ src/Shared/Components/GenericSectionErrorState/GenericSectionErrorState.component.tsx
@@ -1,19 +1,35 @@
 import { ReactComponent as ErrorIcon } from '../../../Assets/Icon/ic-errorInfo.svg'
 import { GenericSectionErrorStateProps } from './types'
 
-const GenericSectionErrorState = ({ reload, withBorder = false }: GenericSectionErrorStateProps) => (
-    <div className={`flex column dc__gap-8 p-16 ${withBorder ? 'dc__border' : ''}`}>
+const GenericSectionErrorState = ({
+    reload,
+    withBorder = false,
+    title = 'Failed to load',
+    subTitle = 'We could not load the information on this page.',
+    description = 'Please reload or try again later',
+    buttonText = 'Reload',
+    rootClassName,
+}: GenericSectionErrorStateProps) => (
+    <div className={`flex column dc__gap-8 p-16 ${withBorder ? 'dc__border' : ''} ${rootClassName || ''}`}>
         <ErrorIcon className="icon-dim-24 alert-icon-r5-imp" />
         <div className="flex column dc__gap-4">
-            <h3 className="fs-13 lh-20 fw-6 cn-9 m-0">Failed to load</h3>
+            <h3 className="fs-13 lh-20 fw-6 cn-9 m-0">{title}</h3>
             <div className="flex column fs-13 lh-20 fw-4 cn-7">
-                <p className="m-0">We could not load the information on this page.</p>
-                <p className="m-0">Please reload or try again later</p>
+                {subTitle && <p className="m-0">{subTitle}</p>}
+                {description && <p className="m-0">{description}</p>}
             </div>
         </div>
-        <button type="button" className="cta text h-20 fs-13 lh-20-imp" onClick={reload}>
-            Reload
-        </button>
+
+        {reload && (
+            <button
+                type="button"
+                className="cta text h-20 fs-13 lh-20-imp"
+                onClick={reload}
+                data-testid="generic-section-reload-button"
+            >
+                {buttonText}
+            </button>
+        )}
     </div>
 )
 
