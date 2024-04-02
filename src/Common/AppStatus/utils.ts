505e015effc813c66b6c087d6eb4a0b99c019774
randhir-devtron <randhir.kumar@devtron.ai>
2024-04-02 17:40:46 +0530

    chore: refactor few components

--- src/Common/AppStatus/utils.ts
+++ src/Common/AppStatus/utils.ts
@@ -1,4 +1,5 @@
 import { TIMELINE_STATUS } from './constants'
+
 export const triggerStatus = (triggerDetailStatus: string): string => {
     const triggerStatus = triggerDetailStatus?.toUpperCase()
     if (triggerStatus === TIMELINE_STATUS.ABORTED || triggerStatus === TIMELINE_STATUS.DEGRADED) {
