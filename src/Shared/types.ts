64b18d8fc8819ef2917349f2e2fec2f20254e7bc
Eshank Vaish <eshank@devtron.ai>
2024-04-15 15:39:16 +0530

    feat: add PatchQueryType enum & PatchOperationType enum

--- src/Shared/types.ts
+++ src/Shared/types.ts
@@ -1,3 +1,5 @@
+import { PatchOperationType } from './constants'
+
 export interface RuntimeParamsAPIResponseType {
     envVariables: Record<string, string>
 }
@@ -10,3 +12,67 @@ export enum CIMaterialSidebarType {
     CODE_SOURCE = 'Code Source',
     PARAMETERS = 'Parameters',
 }
+
+/**
+ * @example Usage with specific enum for path & `unknown` type for value
+ * ```ts
+ * enum PatchKeys {
+ *  name = 'name',
+ *  description = 'description',
+ * }
+ *
+ * const query: PatchQueryType<PatchKeys> = {
+ *  op: PatchOperationType.replace,
+ *  path: PatchKeys.name,
+ *  value: '1'
+ * }
+ * ```
+ *
+ * @example Usage with specific enum for path & custom type for value
+ * ```ts
+ * enum PatchKeys {
+ *  name = 'name',
+ *  description = 'description',
+ * }
+ *
+ * const query: PatchQueryType<PatchKeys, number> = {
+ *  op: PatchOperationType.replace,
+ *  path: PatchKeys.name,
+ *  value: 1
+ * }
+ * ```
+ *
+ * @example Usage with `PatchOperationType.remove`
+ * Note: We cannot add value to remove operation
+ *
+ * ```ts
+ * const query: PatchQueryType<string> = {
+ *  op: PatchOperationType.remove,
+ *  path: 'name'
+ * }
+ * ```
+ */
+export type PatchQueryType<T extends string, K = unknown> = {
+    /**
+     * The path of the json to be patched
+     */
+    path: T
+} & (
+    | {
+          /**
+           * Operation type for patch
+           */
+          op: PatchOperationType.replace
+          /**
+           * Corresponding value for the operation
+           */
+          value: K
+      }
+    | {
+          /**
+           * Operation type for patch
+           */
+          op: PatchOperationType.remove
+          value?: never
+      }
+)
