68b3849b6f8737aed4a2977cb35b030d9de2c439
Amrit Borah <elessar1802@gmail.com>
2024-04-29 20:09:46 +0530

    chore: add css granularity to ScannedByToolModal

--- src/Shared/Components/ScannedByToolModal/ScannedByToolModal.tsx
+++ src/Shared/Components/ScannedByToolModal/ScannedByToolModal.tsx
@@ -1,25 +1,30 @@
+import React from 'react'
 import { IMAGE_SCAN_TOOL, SCAN_TOOL_ID_TRIVY } from '../../constants'
 import { ScannedByToolModalProps } from './types'
 import { ReactComponent as ICClair } from '../../../Assets/Icon/ic-clair.svg'
 import { ReactComponent as ICTrivy } from '../../../Assets/Icon/ic-trivy.svg'
 
-const ScannedByToolModal = ({ scanToolId }: ScannedByToolModalProps) => {
+const ScannedByToolModal: React.FC<ScannedByToolModalProps> = ({
+    scanToolId,
+    fontSize = 13,
+    spacingBetweenTextAndIcon = 6,
+}) => {
     const isTrivy = scanToolId === SCAN_TOOL_ID_TRIVY
 
     return (
-        <>
-            <span className="dc__italic-font-style fs-13">
+        <div className="flexbox" style={{ gap: `${spacingBetweenTextAndIcon}px` }}>
+            <span className={`dc__italic-font-style fs-${fontSize}`}>
                 Scanned by
                 <span className="fw-6 ml-4" data-testid="scanned-by-tool">
                     {isTrivy ? IMAGE_SCAN_TOOL.Trivy : IMAGE_SCAN_TOOL.Clair}
                 </span>
             </span>
             {isTrivy ? (
-                <ICTrivy className="h-20 w-20 ml-6 dc__no-shrink" />
+                <ICTrivy className="icon-dim-20 dc__no-shrink" />
             ) : (
-                <ICClair className="h-20 w-20 ml-6 dc__no-shrink" />
+                <ICClair className="icon-dim-20 dc__no-shrink" />
             )}
-        </>
+        </div>
     )
 }
 
