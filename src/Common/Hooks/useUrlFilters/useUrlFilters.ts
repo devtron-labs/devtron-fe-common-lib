13a744322bd36e2bd2150c75b41ff2aef596df62
Eshank Vaish <eshank@devtron.ai>
2024-05-24 17:04:12 +0530

    fix: history replace in useUrlFilters

--- src/Common/Hooks/useUrlFilters/useUrlFilters.ts
+++ src/Common/Hooks/useUrlFilters/useUrlFilters.ts
@@ -73,6 +73,8 @@ const useUrlFilters = <T = string, K = unknown>({
     const _resetPageNumber = () => {
         if (pageNumber !== DEFAULT_PAGE_NUMBER) {
             _updateSearchParam(PAGE_NUMBER, DEFAULT_PAGE_NUMBER)
+        } else {
+            history.replace({ search: searchParams.toString() })
         }
     }
 
