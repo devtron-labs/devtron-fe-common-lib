a3bd0a79f766d22c27bb5a9d1c98c8c0dd8dfdc8
shivani170 <shivani@devtron.ai>
2024-06-06 23:56:34 +0530

    data select

---
+++ src/Shared/Components/DatePicker/DateSelect.tsx
@@ -0,0 +1,23 @@
+import ReactSelect from 'react-select'
+import { DateSelectProps } from './types'
+import { MONTHLY_DATE_OPTIONS, getTimePickerStyles } from './utils'
+import { DropdownIndicator } from '../ReactSelect/utils'
+
+export const DateSelect = ({ selectedDate, handleOnChange }: DateSelectProps) => (
+    <div className="dc__no-shrink">
+        <ReactSelect
+            placeholder="12:00 AM"
+            options={MONTHLY_DATE_OPTIONS}
+            menuPlacement="auto"
+            components={{
+                IndicatorSeparator: null,
+                ClearIndicator: null,
+                DropdownIndicator,
+            }}
+            isSearchable={false}
+            styles={getTimePickerStyles()}
+            value={selectedDate}
+            onChange={handleOnChange}
+        />
+    </div>
+)
---
+++ src/Shared/Components/DatePicker/index.ts
@@ -0,0 +1,4 @@
+export { default as SingleDatePickerComponent } from './SingleDatePickerComponent'
+export * from './types'
+export * from './DateSelect'
+export * from './TimeSelect'
