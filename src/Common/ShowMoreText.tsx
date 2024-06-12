8597e88a090ee152fafb719b12a73e173623f667
Arun Jain <arun.jain@devtron.ai>
2024-06-12 19:31:24 +0530

    refactor: add deployment history components from dashboard

---
+++ src/Common/ShowMoreText.tsx
@@ -0,0 +1,38 @@
+import { useEffect, useRef, useState } from 'react'
+
+interface ShowMoreTextProps {
+    text: string
+}
+
+export const ShowMoreText = ({ text }: ShowMoreTextProps) => {
+    const ellipsisText = useRef(null)
+    const [showToggle, setShowToggle] = useState(false)
+    const [showAllText, setShowAllText] = useState(false)
+
+    useEffect(() => {
+        if (ellipsisText.current?.offsetHeight && ellipsisText.current?.scrollHeight) {
+            if (ellipsisText.current.offsetHeight === ellipsisText.current?.scrollHeight) {
+                setShowToggle(false)
+            } else {
+                setShowToggle(true)
+            }
+        }
+    }, [ellipsisText.current?.scrollHeight])
+
+    const toggleShowText = () => {
+        setShowAllText(!showAllText)
+    }
+
+    return (
+        <div className="min-w-385 pr-20">
+            <span ref={ellipsisText} className={`${showAllText ? '' : 'dc__truncate '}`}>
+                {text}
+            </span>
+            {showToggle && (
+                <div className="cursor cb-5" onClick={toggleShowText}>{`${
+                    showAllText ? 'Show less' : 'Show more'
+                }`}</div>
+            )}
+        </div>
+    )
+}
