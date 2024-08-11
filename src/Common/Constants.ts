a4701c24adc1d9543f199b73c09db3cb8d0c9c0f
AbhishekA1509 <abhishek@devtron.ai>
2024-08-11 18:56:05 +0530

    fix: revert search working as regex search for logs

--- src/Common/Constants.ts
+++ src/Common/Constants.ts
@@ -34,6 +34,7 @@ export const PATTERNS = {
     KUBERNETES_KEY_NAME: /^(([A-Za-z0-9][-A-Za-z0-9_.]*)?[A-Za-z0-9])$/,
     START_END_ALPHANUMERIC: /^([Az09].*[A-Za-z0-9])$|[A-Za-z0-9]$/,
     ALPHANUMERIC_WITH_SPECIAL_CHAR: /^[A-Za-z0-9._-]+$/, // allow alphanumeric,(.) ,(-),(_)
+    ESCAPED_CHARACTERS: /[.*+?^${}()|[\]\\]/g,
 }
 
 export const URLS = {
