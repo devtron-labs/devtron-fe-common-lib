4be648228a43ae959c001c926d02b90941e75c56
shivani170 <shivani@devtron.ai>
2024-08-29 16:51:40 +0530

    chore: fix for error handling in artifact

--- src/Shared/Components/CICDHistory/Artifacts.tsx
+++ src/Shared/Components/CICDHistory/Artifacts.tsx
@@ -188,7 +188,11 @@ const Artifacts = ({
             </div>
         )
     }
-    if (status.toLowerCase() === TERMINAL_STATUS_MAP.FAILED || status.toLowerCase() === TERMINAL_STATUS_MAP.CANCELLED) {
+    if (
+        status.toLowerCase() === TERMINAL_STATUS_MAP.FAILED ||
+        status.toLowerCase() === TERMINAL_STATUS_MAP.CANCELLED ||
+        status.toLowerCase() === TERMINAL_STATUS_MAP.ERROR
+    ) {
         if (isJobCI) {
             return (
                 <GenericEmptyState
