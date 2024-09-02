cfa69ab3bfe9f4920db3a9db6e3a71a98eabcd6d
Eshank Vaish <eshank@devtron.ai>
2024-09-02 19:30:19 +0530

    fix: child visibility for loading state

--- src/Shared/Components/Button/Button.component.tsx
+++ src/Shared/Components/Button/Button.component.tsx
@@ -16,8 +16,7 @@ const Button = ({
     disabled,
     isLoading,
 }: ButtonProps) => {
-    const visibilityClass = isLoading ? 'dc__visibility-hidden' : ''
-    const iconClass = `dc__no-shrink flex dc__fill-available-space ${BUTTON_SIZE_TO_ICON_CLASS_NAME_MAP[size]} ${visibilityClass}`
+    const iconClass = `dc__no-shrink flex dc__fill-available-space ${BUTTON_SIZE_TO_ICON_CLASS_NAME_MAP[size]}`
 
     return (
         <button
@@ -25,10 +24,10 @@ const Button = ({
             disabled={disabled || isLoading}
             // eslint-disable-next-line react/button-has-type
             type={buttonProps.type || 'button'}
-            className={`br-4 flex cursor dc__mnw-100 dc__tab-focus dc__position-rel dc__capitalize ${getButtonDerivedClass({ size, variant, style })} ${disabled ? 'dc__disabled' : ''} ${buttonProps.className || ''}`}
+            className={`br-4 flex cursor dc__mnw-100 dc__tab-focus dc__position-rel dc__capitalize ${getButtonDerivedClass({ size, variant, style, isLoading })} ${disabled ? 'dc__disabled' : ''} ${buttonProps.className || ''}`}
         >
             {startIcon && <span className={iconClass}>{startIcon}</span>}
-            <span className={`dc__mxw-150 dc__align-left dc__truncate ${visibilityClass}`}>{text}</span>
+            <span className="dc__mxw-150 dc__align-left dc__truncate">{text}</span>
             {endIcon && <span className={iconClass}>{endIcon}</span>}
             {isLoading && <Progressing size={BUTTON_SIZE_TO_LOADER_SIZE_MAP[size]} />}
         </button>
