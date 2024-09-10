23fdfa0187378a58a33684d36a2c127739a91f96
Eshank Vaish <eshank@devtron.ai>
2024-09-10 23:19:43 +0530

    fix: min width for button and code clean up

--- src/Shared/Components/Button/utils.ts
+++ src/Shared/Components/Button/utils.ts
@@ -1,27 +1,23 @@
 import {
     BUTTON_SIZE_TO_CLASS_NAME_MAP,
-    BUTTON_SIZE_TO_ICON_CLASS_NAME_MAP,
-    BUTTON_SIZE_TO_LOADER_SIZE_MAP,
+    BUTTON_SIZE_TO_ICON_SIZE_MAP,
     ICON_BUTTON_SIZE_TO_CLASS_NAME_MAP,
-    ICON_BUTTON_SIZE_TO_ICON_CLASS_NAME_MAP,
-    ICON_BUTTON_SIZE_TO_LOADER_SIZE_MAP,
+    ICON_BUTTON_SIZE_TO_ICON_SIZE_MAP,
 } from './constants'
 import { ButtonProps } from './types'
 
 export const getButtonIconClassName = ({ size, icon }: Pick<ButtonProps, 'size' | 'icon'>) => {
-    if (icon) {
-        return ICON_BUTTON_SIZE_TO_ICON_CLASS_NAME_MAP[size]
-    }
+    const iconSize = icon ? ICON_BUTTON_SIZE_TO_ICON_SIZE_MAP[size] : BUTTON_SIZE_TO_ICON_SIZE_MAP[size]
 
-    return BUTTON_SIZE_TO_ICON_CLASS_NAME_MAP[size]
+    return `icon-dim-${iconSize}`
 }
 
 export const getButtonLoaderSize = ({ size, icon }: Pick<ButtonProps, 'size' | 'icon'>) => {
     if (icon) {
-        return ICON_BUTTON_SIZE_TO_LOADER_SIZE_MAP[size]
+        return ICON_BUTTON_SIZE_TO_ICON_SIZE_MAP[size]
     }
 
-    return BUTTON_SIZE_TO_LOADER_SIZE_MAP[size]
+    return BUTTON_SIZE_TO_ICON_SIZE_MAP[size]
 }
 
 export const getButtonDerivedClass = ({
@@ -31,4 +27,4 @@ export const getButtonDerivedClass = ({
     isLoading,
     icon,
 }: Pick<ButtonProps, 'variant' | 'size' | 'style' | 'isLoading' | 'icon'>) =>
-    `button button__${variant}--${style} ${icon ? ICON_BUTTON_SIZE_TO_CLASS_NAME_MAP[size] : `${BUTTON_SIZE_TO_CLASS_NAME_MAP[size]} mw-64`} ${isLoading ? 'button--loading' : ''}`
+    `button button__${variant}--${style} ${icon ? ICON_BUTTON_SIZE_TO_CLASS_NAME_MAP[size] : BUTTON_SIZE_TO_CLASS_NAME_MAP[size]} ${isLoading ? 'button--loading' : ''}`
