131f554a8f8eb575298ff1556e3ab4b181ec5cc2
AbhishekA1509 <abhishek@devtron.ai>
2024-10-04 13:26:56 +0530

    feat: add InvalidYAMLTooltip

---
+++ src/Shared/Components/InvalidYAMLTippy/InvalidYAMLTippyWrapper.tsx
@@ -0,0 +1,31 @@
+import { Tooltip } from '@Common/Tooltip'
+import { InvalidYAMLTippyWrapperProps } from './types'
+import { getInvalidTippyContent } from './utils'
+
+const InvalidYAMLTippy = ({ parsingError, restoreLastSavedYAML, children }: InvalidYAMLTippyWrapperProps) => (
+    <Tooltip
+        alwaysShowTippyOnHover
+        interactive
+        content={getInvalidTippyContent({
+            parsingError,
+            restoreLastSavedYAML,
+        })}
+    >
+        {children}
+    </Tooltip>
+)
+
+const InvalidYAMLTippyWrapper = ({ parsingError, restoreLastSavedYAML, children }: InvalidYAMLTippyWrapperProps) => {
+    if (parsingError) {
+        return (
+            <InvalidYAMLTippy parsingError={parsingError} restoreLastSavedYAML={restoreLastSavedYAML}>
+                {children}
+            </InvalidYAMLTippy>
+        )
+    }
+
+    // eslint-disable-next-line react/jsx-no-useless-fragment
+    return <>{children}</>
+}
+
+export default InvalidYAMLTippyWrapper
