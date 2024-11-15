0d8767a991368eb3fecf680b8b8617430f088135
Amrit Kashyap Borah <amrit.borah@devtron.ai>
2024-11-15 16:21:33 +0530

    fix!: unable to copy from safari in clipboard button

--- src/Common/ClipboardButton/ClipboardButton.tsx
+++ src/Common/ClipboardButton/ClipboardButton.tsx
@@ -14,9 +14,9 @@
  * limitations under the License.
  */
 
-import { useState, useEffect, useCallback } from 'react'
-import Tippy from '@tippyjs/react'
+import { useState, useEffect, useRef } from 'react'
 import { copyToClipboard, noop, stopPropagation } from '../Helper'
+import Tooltip from '@Common/Tooltip/Tooltip'
 import ClipboardProps from './types'
 import { ReactComponent as ICCopy } from '../../Assets/Icon/ic-copy.svg'
 import { ReactComponent as Check } from '../../Assets/Icon/ic-check.svg'
@@ -25,8 +25,7 @@ import { ReactComponent as Check } from '../../Assets/Icon/ic-check.svg'
  * @param content - Content to be copied
  * @param copiedTippyText - Text to be shown in the tippy when the content is copied, default 'Copied!'
  * @param duration - Duration for which the tippy should be shown, default 1000
- * @param trigger - To trigger the copy action outside the button, if set to true the content will be copied, use case being triggering the copy action from outside the component
- * @param setTrigger - Callback function to set the trigger outside the button
+ * @param copyToClipboardPromise - the promise returned by copyToClipboard util function
  * @param rootClassName - additional classes to add to button
  * @param iconSize - size of svg icon to be shown, default 16 (icon-dim-16)
  */
@@ -34,64 +33,79 @@ export default function ClipboardButton({
     content,
     copiedTippyText = 'Copied!',
     duration = 1000,
-    trigger,
-    setTrigger = noop,
+    copyToClipboardPromise,
     rootClassName = '',
     iconSize = 16,
 }: ClipboardProps) {
     const [copied, setCopied] = useState<boolean>(false)
-    const [enableTippy, setEnableTippy] = useState<boolean>(false)
+    const setCopiedFalseTimeoutRef = useRef<ReturnType<typeof setTimeout>>(-1)
 
-    const handleTextCopied = () => {
+    const handleTriggerCopy = () => {
         setCopied(true)
-    }
-    const isTriggerUndefined = typeof trigger === 'undefined'
 
-    const handleEnableTippy = () => setEnableTippy(true)
-    const handleDisableTippy = () => setEnableTippy(false)
-    const handleCopyContent = useCallback(
-        (e?) => {
-            if (e) stopPropagation(e)
-            copyToClipboard(content, handleTextCopied)
-        },
-        [content],
-    )
+        setCopiedFalseTimeoutRef.current = setTimeout(() => {
+            setCopied(false)
+
+            setCopiedFalseTimeoutRef.current = -1
+        }, duration)
+    }
+
+    const handleAwaitCopyToClipboardPromise = async (shouldRunCopy?: boolean) => {
+        try {
+            if (shouldRunCopy) {
+                await copyToClipboard(content)
+            } else {
+                await copyToClipboardPromise
+            }
+
+            handleTriggerCopy()
+        } catch {
+            noop()
+        }
+    }
+
+    const handleCopyContent = async (e?: React.MouseEvent) => {
+        if (e) {
+            stopPropagation(e)
+        }
+
+        await handleAwaitCopyToClipboardPromise(true)
+    }
+
+    useEffect(() => {
+        if (!copyToClipboardPromise) {
+            return
+        }
+
+        handleAwaitCopyToClipboardPromise().catch(noop)
+    }, [copyToClipboardPromise])
+
+    useEffect(() => {
+        return () => {
+            if (setCopiedFalseTimeoutRef.current > -1) {
+                clearTimeout(setCopiedFalseTimeoutRef.current)
+            }
+        }
+    }, [])
+
     const iconClassName = `icon-dim-${iconSize} dc__no-shrink`
 
-    useEffect(() => {
-        if (!copied) return
-
-        const timeout = setTimeout(() => {
-            setCopied(false)
-            setTrigger(false)
-        }, duration)
-
-        return () => clearTimeout(timeout)
-    }, [copied, duration, setTrigger])
-
-    useEffect(() => {
-        if (!isTriggerUndefined && trigger) {
-            setCopied(true)
-            handleCopyContent()
-        }
-    }, [trigger, handleCopyContent])
     return (
-        <Tippy
-            className="default-tt"
-            content={copied ? copiedTippyText : 'Copy'}
-            placement="bottom"
-            visible={copied || enableTippy}
-            arrow={false}
+        <Tooltip
+            content={'Copy'}
+            alwaysShowTippyOnHover={!copied}
         >
             <button
                 type="button"
                 className={`dc__outline-none-imp p-0 flex dc__transparent--unstyled dc__no-border ${rootClassName}`}
-                onMouseEnter={handleEnableTippy}
-                onMouseLeave={handleDisableTippy}
-                onClick={isTriggerUndefined ? handleCopyContent : noop}
+                onClick={handleCopyContent}
             >
-                {copied ? <Check className={iconClassName} /> : <ICCopy className={iconClassName} />}
+                <Tooltip content={copiedTippyText} alwaysShowTippyOnHover visible={copied}>
+                    <div className="flex">
+                        {copied ? <Check className={iconClassName} /> : <ICCopy className={iconClassName} />}
+                    </div>
+                </Tooltip>
             </button>
-        </Tippy>
+        </Tooltip>
     )
 }
