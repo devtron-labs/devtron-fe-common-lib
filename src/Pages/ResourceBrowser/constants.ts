c72fdda3018420c1ddb121318c7f1ab96abab0ef
AbhishekA1509 <abhishek@devtron.ai>
2024-12-16 17:34:10 +0530

    fix: add type to ALL_NAMESPACE_OPTION

--- src/Pages/ResourceBrowser/constants.ts
+++ src/Pages/ResourceBrowser/constants.ts
@@ -1,1 +1,6 @@
-export const ALL_NAMESPACE_OPTION = { value: 'all', label: 'All namespaces' }
+import { SelectPickerOptionType } from '@Shared/Components'
+
+export const ALL_NAMESPACE_OPTION: Readonly<Pick<SelectPickerOptionType, 'value' | 'label'>> = {
+    value: 'all',
+    label: 'All namespaces',
+}
