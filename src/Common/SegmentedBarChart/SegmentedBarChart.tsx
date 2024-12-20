3e43ad0265b190a77a004ffa5cbfb26ca7825538
shivani170 <shivani@devtron.ai>
2024-12-20 15:16:36 +0530

    chore:  version bump 1.3.0-beta-13

--- src/Common/SegmentedBarChart/SegmentedBarChart.tsx
+++ src/Common/SegmentedBarChart/SegmentedBarChart.tsx
@@ -39,43 +39,43 @@ const SegmentedBarChart: React.FC<SegmentedBarChartProps> = ({
 
     const renderValue = (value: string | number, label: string) => (
         <span className={countClassName} data-testid={`segmented-bar-chart-${label}-value`}>
-            {value}/{total}
+            {isProportional ? `${value}/${total}` : value}
         </span>
     )
 
-    const renderContent = (entity: Entity) => {
+    const renderContent = () => {
         if (isProportional) {
-            if (entity.value === 0) {
-                return null
-            }
-            return (
-                <>
-                    {renderValue(entity.value, entity.label)}
-                    <div className="flex left dc__gap-6">
-                        <span style={{ backgroundColor: entity.color }} className="h-12 dc__border-radius-2 w-4" />
-                        {renderLabel(entity.label)}
+            return entities?.map((entity) => {
+                if (entity.value === 0) {
+                    return null
+                }
+                return (
+                    <div className="flexbox-col">
+                        {renderValue(entity.value, entity.label)}
+                        <div className="flex left dc__gap-6">
+                            <span style={{ backgroundColor: entity.color }} className="h-12 dc__border-radius-2 w-4" />
+                            {renderLabel(entity.label)}
+                        </div>
                     </div>
-                </>
-            )
+                )
+            })
         }
-        return (
-            <>
+        return entities?.map((entity) => (
+            <div className={`${isProportional ? 'flexbox-col' : 'flexbox  dc__gap-4 dc__align-items-center'}`}>
                 <div className="dot" style={{ backgroundColor: entity.color, width: '10px', height: '10px' }} />
                 {renderValue(entity.value, entity.label)}
                 {renderLabel(entity.label)}
-            </>
-        )
+            </div>
+        ))
+    }
+
+    if (!entities.length) {
+        return null
     }
 
     return (
         <div className={`flexbox-col w-100 dc__gap-12 ${rootClassName}`}>
-            <div className={`flexbox ${isProportional ? 'dc__gap-24' : 'dc__gap-16'}`}>
-                {entities?.map((entity) => (
-                    <div className={`${isProportional ? 'flexbox-col' : 'flexbox  dc__gap-4 dc__align-items-center'}`}>
-                        {renderContent(entity)}
-                    </div>
-                ))}
-            </div>
+            <div className={`flexbox ${isProportional ? 'dc__gap-24' : 'dc__gap-16'}`}>{renderContent()}</div>
             <div className="flexbox dc__gap-2">
                 {filteredEntities?.map((entity, index, map) => (
                     <div
