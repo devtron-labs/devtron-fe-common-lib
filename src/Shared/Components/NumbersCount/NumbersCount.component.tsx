6b47c1e2f3e7914a02f51b03f5b5851177bfb8d6
Rohit Raj <rohit.raj@devtron.ai>
2024-12-27 18:40:59 +0530

    feat: NumbersCount - remove isSelected style change

--- src/Shared/Components/NumbersCount/NumbersCount.component.tsx
+++ src/Shared/Components/NumbersCount/NumbersCount.component.tsx
@@ -1,10 +1,8 @@
 import { ReactComponent as ICTilde } from '@Icons/ic-tilde.svg'
 import { NumbersCountProps } from './types'
 
-const NumbersCount = ({ count, isApprox, isSelected }: NumbersCountProps) => (
-    <div
-        className={`flex dc__no-shrink br-12 dc__gap-2 px-6 ${isSelected ? 'bcb-5 cn-0' : 'bcn-1 cn-7'} fs-13 fw-6 lh-20`}
-    >
+const NumbersCount = ({ count, isApprox }: NumbersCountProps) => (
+    <div className="flex dc__no-shrink br-12 dc__gap-2 px-6 bcn-1 cn-7 fs-13 fw-6 lh-20">
         {isApprox && <ICTilde className="dc__no-shrink icon-dim-12 scn-0" />}
         <span>{count}</span>
     </div>
--- src/Shared/Components/NumbersCount/types.ts
+++ src/Shared/Components/NumbersCount/types.ts
@@ -2,11 +2,6 @@ export interface NumbersCountProps {
     count: number
     /**
      * @default false
-     * @description If true, it will change the bgColor to var(--B500)
-     */
-    isSelected?: boolean
-    /**
-     * @default false
      * @description If true, it will show the count with tilde like ~number
      */
     isApprox?: boolean
