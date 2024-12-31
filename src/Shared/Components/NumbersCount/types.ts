bcd14bb946b7d39eb5ece9f7974f7b9f0f9130c8
Rohit Raj <rohit.raj@devtron.ai>
2024-12-31 13:26:47 +0530

    feat: revert isSelected prop in NumbersCount, refactor: VirtualizedList - types refactor

--- src/Shared/Components/NumbersCount/types.ts
+++ src/Shared/Components/NumbersCount/types.ts
@@ -2,6 +2,11 @@ export interface NumbersCountProps {
     count: number
     /**
      * @default false
+     * @description If true, it will change the bgColor to var(--B500)
+     */
+    isSelected?: boolean
+    /**
+     * @default false
      * @description If true, it will show the count with tilde like ~number
      */
     isApprox?: boolean
--- src/Shared/Components/VirtualizedList/VirtualizedList.types.ts
+++ src/Shared/Components/VirtualizedList/VirtualizedList.types.ts
@@ -1,4 +1,4 @@
-import { AutoSizedStickyTreeProps, StickyTreeRowRenderer, TreeNode } from 'react-virtualized-sticky-tree'
+import { AutoSizedStickyTreeProps, StickyTreeRowRenderer } from 'react-virtualized-sticky-tree'
 
 /**
  * Represents a virtualized list item.
@@ -6,15 +6,16 @@ import { AutoSizedStickyTreeProps, StickyTreeRowRenderer, TreeNode } from 'react
  * @template ListKeys - The type of the keys used to identify list items (e.g., `string` or `number`).
  * @template ExtendedType - An optional type to extend the base item structure with additional fields.
  */
-export type VirtualizedListItem<ListKeys, ExtendedType = {}> = TreeNode &
-    ExtendedType & {
-        /** The height of the item. (in pixels) */
-        height: number
-        /** The depth level of the item in the hierarchical structure. */
-        depth: number
-        /** An optional array of child item keys. */
-        children?: ListKeys[]
-    }
+export type VirtualizedListItem<ListKeys, ExtendedType = {}> = ExtendedType & {
+    /** The unique identifier of the item. */
+    id: string | number
+    /** The height of the item. (in pixels) */
+    height: number
+    /** The depth level of the item in the hierarchical structure. */
+    depth: number
+    /** An optional array of child item keys. */
+    children?: ListKeys[]
+}
 
 /**
  * Props for the VirtualizedList component, which supports a virtualized tree-like structure.
