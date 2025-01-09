7adf006f1c250c5bc7dc73df28bd0b233e6ce5ee
Eshank Vaish <48060426+eshankvaish@users.noreply.github.com>
2025-01-10 00:32:05 +0530

    feat: add tooltip for theme switcher

--- src/Shared/Components/ThemeSwitcher/ThemeSwitcher.component.tsx
+++ src/Shared/Components/ThemeSwitcher/ThemeSwitcher.component.tsx
@@ -1,8 +1,8 @@
 import ReactGA from 'react-ga4'
 import { useTheme } from '@Shared/Providers'
 import { THEME_PREFERENCE_MAP, ThemePreferenceType } from '@Shared/Providers/ThemeProvider/types'
-import { SegmentedControl, SegmentedControlVariant } from '@Common/SegmentedControl'
-import { ChangeEvent } from 'react'
+import { SegmentedControl, SegmentedControlProps, SegmentedControlVariant } from '@Common/SegmentedControl'
+import { ChangeEvent, useMemo } from 'react'
 import { stopPropagation } from '@Common/Helper'
 import { ComponentSizeType } from '@Shared/constants'
 import { THEME_PREFERENCE_TO_ICON_MAP } from './constants'
@@ -11,18 +11,25 @@ import { ThemeSwitcherProps } from './types'
 const ThemeSwitcher = ({ onChange }: ThemeSwitcherProps) => {
     const { themePreference, handleSelectedThemeChange } = useTheme()
 
+    const { tabs, tooltips } = useMemo<Required<Pick<SegmentedControlProps, 'tabs' | 'tooltips'>>>(() => {
+        const availableThemePreferences = Object.values(THEME_PREFERENCE_MAP)
+
+        return {
+            tabs: availableThemePreferences.map((value) => ({
+                label: (
+                    <span className="dc__no-shrink icon-dim-20 flex dc__fill-available-space">
+                        {THEME_PREFERENCE_TO_ICON_MAP[value].icon}
+                    </span>
+                ),
+                value,
+            })),
+            tooltips: availableThemePreferences.map((value) => THEME_PREFERENCE_TO_ICON_MAP[value].tippyContent),
+        }
+    }, [])
+
     if (!window._env_.FEATURE_EXPERIMENTAL_THEMING_ENABLE) {
         return null
     }
-
-    const tabs = Object.values(THEME_PREFERENCE_MAP).map((value) => ({
-        label: (
-            <span className="dc__no-shrink icon-dim-20 flex dc__fill-available-space">
-                {THEME_PREFERENCE_TO_ICON_MAP[value]}
-            </span>
-        ),
-        value,
-    }))
 
     const handleThemeSwitch = (e: ChangeEvent<HTMLInputElement>) => {
         const updatedThemePreference = e.target.value as ThemePreferenceType
@@ -44,6 +51,7 @@ const ThemeSwitcher = ({ onChange }: ThemeSwitcherProps) => {
                 name="theme-preference-selector"
                 onChange={handleThemeSwitch}
                 tabs={tabs}
+                tooltips={tooltips}
                 size={ComponentSizeType.large}
                 variant={SegmentedControlVariant.GRAY_ON_WHITE}
             />
--- src/Shared/Components/ThemeSwitcher/constants.tsx
+++ src/Shared/Components/ThemeSwitcher/constants.tsx
@@ -4,8 +4,23 @@ import { ReactComponent as ICSun } from '@Icons/ic-sun.svg'
 import { ReactComponent as ICMoon } from '@Icons/ic-moon.svg'
 import { ReactComponent as ICLaptop } from '@Icons/ic-laptop.svg'
 
-export const THEME_PREFERENCE_TO_ICON_MAP: Record<ThemePreferenceType, ReactElement> = {
-    [AppThemeType.light]: <ICSun className="scn-9" />,
-    [AppThemeType.dark]: <ICMoon className="scn-9" />,
-    [THEME_PREFERENCE_MAP.auto]: <ICLaptop className="scn-9" />,
+export const THEME_PREFERENCE_TO_ICON_MAP: Record<
+    ThemePreferenceType,
+    {
+        tippyContent: string
+        icon: ReactElement
+    }
+> = {
+    [AppThemeType.light]: {
+        tippyContent: 'Light',
+        icon: <ICSun className="scn-9" />,
+    },
+    [AppThemeType.dark]: {
+        tippyContent: 'Dark',
+        icon: <ICMoon className="scn-9" />,
+    },
+    [THEME_PREFERENCE_MAP.auto]: {
+        tippyContent: 'System',
+        icon: <ICLaptop className="scn-9" />,
+    },
 } as const
