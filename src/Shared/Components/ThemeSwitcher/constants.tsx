bbb98bad73539e7bd0cdc5f3fe98ebb47b7dc27f
Eshank Vaish <48060426+eshankvaish@users.noreply.github.com>
2025-01-10 00:31:22 +0530

    feat: update the icon for auto theme

--- src/Shared/Components/ThemeSwitcher/constants.tsx
+++ src/Shared/Components/ThemeSwitcher/constants.tsx
@@ -2,9 +2,10 @@ import { ReactElement } from 'react'
 import { AppThemeType, THEME_PREFERENCE_MAP, ThemePreferenceType } from '@Shared/Providers/ThemeProvider/types'
 import { ReactComponent as ICSun } from '@Icons/ic-sun.svg'
 import { ReactComponent as ICMoon } from '@Icons/ic-moon.svg'
+import { ReactComponent as ICLaptop } from '@Icons/ic-laptop.svg'
 
 export const THEME_PREFERENCE_TO_ICON_MAP: Record<ThemePreferenceType, ReactElement> = {
     [AppThemeType.light]: <ICSun className="scn-9" />,
     [AppThemeType.dark]: <ICMoon className="scn-9" />,
-    [THEME_PREFERENCE_MAP.auto]: <span className="bcn-1" />,
+    [THEME_PREFERENCE_MAP.auto]: <ICLaptop className="scn-9" />,
 } as const
