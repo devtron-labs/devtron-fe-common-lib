dd5ce612b07a7e029965ea86736e1a8207d71d42
Eshank Vaish <48060426+eshankvaish@users.noreply.github.com>
2025-01-11 00:35:25 +0530

    fix: storage key for theme preference

--- src/Shared/Providers/ThemeProvider/constants.ts
+++ src/Shared/Providers/ThemeProvider/constants.ts
@@ -1,3 +1,3 @@
-export const THEME_PREFERENCE_STORAGE_KEY = 'selected-theme'
+export const THEME_PREFERENCE_STORAGE_KEY = 'themePreference'
 
 export const DARK_COLOR_SCHEME_MATCH_QUERY = '(prefers-color-scheme: dark)'
