8eda64d4af6f2cb3ed7f6af2e3c9f5caff35a1e4
Eshank Vaish <48060426+eshankvaish@users.noreply.github.com>
2025-01-16 14:38:52 +0530

    feat: add style override for singleValue

--- src/Shared/Components/SelectPicker/ReactSelect.tsx
+++ src/Shared/Components/SelectPicker/ReactSelect.tsx
@@ -4,7 +4,7 @@ import { commonSelectStyles } from '../ReactSelect'
 export interface ReactSelectProps extends Omit<SelectProps, 'styles'> {
     styleOverrides?: Pick<
         SelectProps['styles'],
-        'control' | 'option' | 'valueContainer' | 'dropdownIndicator' | 'menuList' | 'menu'
+        'control' | 'option' | 'valueContainer' | 'dropdownIndicator' | 'menuList' | 'menu' | 'singleValue'
     >
 }
 
@@ -15,31 +15,36 @@ const ReactSelect = ({ styleOverrides, ...props }: ReactSelectProps) => (
             ...commonSelectStyles,
             control: (base, state) => ({
                 ...commonSelectStyles.control(base, state),
-                ...styleOverrides?.control?.(base, state),
+                ...styleOverrides?.control?.({}, state),
             }),
             option: (base, state) => ({
                 ...commonSelectStyles.option(base, state),
-                ...styleOverrides?.option?.(base, state),
+                ...styleOverrides?.option?.({}, state),
             }),
             valueContainer: (base, state) => ({
                 ...commonSelectStyles.valueContainer(base, state),
-                ...styleOverrides?.valueContainer?.(base, state),
+                ...styleOverrides?.valueContainer?.({}, state),
             }),
             dropdownIndicator: (base, state) => ({
                 ...commonSelectStyles.dropdownIndicator(base, state),
-                ...styleOverrides?.dropdownIndicator?.(base, state),
+                ...styleOverrides?.dropdownIndicator?.({}, state),
             }),
             menuList: (base, state) => ({
                 ...commonSelectStyles.menuList(base, state),
-                ...styleOverrides?.menuList?.(base, state),
+                ...styleOverrides?.menuList?.({}, state),
             }),
             menu: (base, state) => ({
                 ...commonSelectStyles.menu(base, state),
-                ...styleOverrides?.menu?.(base, state),
+                ...styleOverrides?.menu?.({}, state),
+            }),
+            singleValue: (base, state) => ({
+                ...commonSelectStyles.singleValue(base, state),
+                ...styleOverrides?.singleValue?.({}, state),
             }),
         }}
     />
 )
 
 export { ReactSelect }
-export type { GroupBase, OptionsOrGroups, SelectInstance, StylesConfig, components } from 'react-select'
+export { components } from 'react-select'
+export type { GroupBase, OptionsOrGroups, SelectInstance, StylesConfig } from 'react-select'
