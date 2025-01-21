10a9df48cca081e747bdd4242766b41e4395a0d5
Eshank Vaish <48060426+eshankvaish@users.noreply.github.com>
2025-01-21 13:51:32 +0530

    fix: icon for help and hover color for select border

--- src/Shared/Components/FormFieldWrapper/FormFieldInfo.tsx
+++ src/Shared/Components/FormFieldWrapper/FormFieldInfo.tsx
@@ -6,7 +6,7 @@ import { FormFieldInfoProps, FormInfoItemProps } from './types'
 const FormInfoItem = ({ id, text, icon, textClass }: FormInfoItemProps) => (
     <div className="flex left dc__gap-4 fs-11 lh-16 fw-4" id={id}>
         <span className="dc__no-shrink icon-dim-16 flex top dc__fill-available-space">{icon}</span>
-        <span className={`dc__ellipsis-right__2nd-line cr-5 ${textClass}`}>{text}</span>
+        <span className={`dc__ellipsis-right__2nd-line ${textClass}`}>{text}</span>
     </div>
 )
 
@@ -16,7 +16,7 @@ const FormFieldInfo = ({ error, helperText, warningText, inputId }: FormFieldInf
         {helperText && (
             <FormInfoItem
                 text={helperText}
-                icon={<ICInfoFilledOverride />}
+                icon={<ICInfoFilledOverride className="info-icon-n7" />}
                 textClass="cn-7"
                 id={`${inputId}-helper-text`}
             />
