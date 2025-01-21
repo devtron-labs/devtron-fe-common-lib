3f02f2f3d67679e78fcf8aeec882e021003d20ce
Eshank Vaish <48060426+eshankvaish@users.noreply.github.com>
2025-01-21 18:32:23 +0530

    fix: transition and code comments

--- src/Shared/Components/Textarea/types.ts
+++ src/Shared/Components/Textarea/types.ts
@@ -4,15 +4,21 @@ import { FormFieldWrapperProps } from '../FormFieldWrapper'
 
 export interface TextareaProps
     extends Omit<FormFieldWrapperProps, 'children' | 'inputId'>,
-        Pick<TextareaHTMLAttributes<HTMLTextAreaElement>, 'onBlur' | 'disabled' | 'autoFocus'>,
-        Required<Pick<TextareaHTMLAttributes<HTMLTextAreaElement>, 'placeholder' | 'onChange' | 'value'>> {
-    name: string
+        Pick<TextareaHTMLAttributes<HTMLTextAreaElement>, 'onBlur' | 'disabled' | 'autoFocus' | 'onFocus'>,
+        Required<Pick<TextareaHTMLAttributes<HTMLTextAreaElement>, 'placeholder' | 'onChange' | 'value' | 'name'>> {
+    /**
+     * Test id for the textarea
+     */
     dataTestId: string
     /**
+     * If false, the input is not trimmed on blur
+     *
      * @default true
      */
     shouldTrim?: boolean
     /**
+     * Size of the textarea
+     *
      * @default ComponentSizeType.large
      */
     size?: Extract<ComponentSizeType, ComponentSizeType.medium | ComponentSizeType.large>
