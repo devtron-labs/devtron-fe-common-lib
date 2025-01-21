52d4c0ebd1f022dd04bdd0a76f922de9be941553
Eshank Vaish <48060426+eshankvaish@users.noreply.github.com>
2025-01-21 15:58:24 +0530

    feat: add support for row layout

--- src/Shared/Components/FormFieldWrapper/FormFieldWrapper.tsx
+++ src/Shared/Components/FormFieldWrapper/FormFieldWrapper.tsx
@@ -14,15 +14,19 @@ const FormFieldWrapper = ({
     children,
 }: Required<FormFieldWrapperProps>) => {
     const isRowLayout = layout === 'row'
+    const itemContainerClassName = isRowLayout ? 'dc__mxw-250 w-100 mxh-36 dc__align-self-stretch' : ''
 
     return (
-        <div className={`flex left top dc__gap-6 ${!isRowLayout ? 'column' : ''} ${fullWidth ? 'w-100' : ''}`}>
-            <div className={isRowLayout ? 'w-250' : ''}>
-                <FormFieldLabel inputId={inputId} label={label} required={required} />
-                {isRowLayout && <div />}
-            </div>
-            <div className="flex column left top dc__gap-4 w-100">
+        <div className={`flex left column top dc__gap-4 ${fullWidth ? 'w-100' : ''}`}>
+            <div className={`flex left top dc__gap-6 ${!isRowLayout ? 'column' : ''} w-100`}>
+                <div className={`${itemContainerClassName} flex left`}>
+                    <FormFieldLabel inputId={inputId} label={label} required={required} />
+                </div>
                 <div className="w-100">{children}</div>
+            </div>
+            <div className="flex left dc__gap-6 w-100">
+                {/* Added a hidden div for layout sync */}
+                {isRowLayout && <div className={`${itemContainerClassName} dc__visibility-hidden`} />}
                 <FormFieldInfo inputId={inputId} error={error} helperText={helperText} warningText={warningText} />
             </div>
         </div>
