75b093d5fe51d52400e1f01ed6f652e52379f1e8
Eshank Vaish <48060426+eshankvaish@users.noreply.github.com>
2025-01-23 00:12:16 +0530

    fix: alignment issue for form info item

--- src/Shared/Components/FormFieldWrapper/FormFieldInfo.tsx
+++ src/Shared/Components/FormFieldWrapper/FormFieldInfo.tsx
@@ -5,7 +5,7 @@ import { FormFieldInfoProps, FormInfoItemProps } from './types'
 import { getFormErrorElementId } from './utils'
 
 const FormInfoItem = ({ id, text, icon, textClass }: FormInfoItemProps) => (
-    <div className="flex left dc__gap-4 fs-11 lh-16 fw-4" id={id}>
+    <div className="flexbox dc__gap-4 fs-11 lh-16 fw-4" id={id}>
         <span className="dc__no-shrink icon-dim-16 flex top dc__fill-available-space">{icon}</span>
         <span className={`dc__ellipsis-right__2nd-line ${textClass}`}>{text}</span>
     </div>
