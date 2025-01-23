1b6194c9b15ece04d9f50a4a204b1ffbca220bb8
AbhishekA1509 <abhishek@devtron.ai>
2025-01-23 18:46:01 +0530

    refactor: update SVG icons for improved design consistency; replace user-circle icon with person icon and adjust icon sizes in tooltip and commit info components

--- src/Shared/Components/CICDHistory/BuildAndTaskSummaryTooltipCard.tsx
+++ src/Shared/Components/CICDHistory/BuildAndTaskSummaryTooltipCard.tsx
@@ -1,12 +1,14 @@
 import { memo } from 'react'
 import { Tooltip } from '@Common/Tooltip'
 import { ReactComponent as ICCalendar } from '@Icons/ic-calendar.svg'
-import { ReactComponent as ICUserCircle } from '@Icons/ic-user-circle.svg'
+import { ReactComponent as ICPerson } from '@Icons/ic-person.svg'
 import GitTriggerList from './GitTriggerList'
 import { DEFAULT_CLUSTER_ID } from './constants'
 import { getFormattedTriggerTime, sanitizeWorkflowExecutionStages } from './utils'
 import { BuildAndTaskSummaryTooltipCardProps } from './types'
 import WorkerStatus from './WorkerStatus'
+
+const BASE_ICON_CLASS = 'icon-dim-20 dc__no-shrink'
 
 const BuildAndTaskSummaryTooltipCard = memo(
     ({
@@ -24,12 +26,12 @@ const BuildAndTaskSummaryTooltipCard = memo(
             <div className="shadow__overlay p-16 br-4 w-350 bg__primary mxh-300 dc__overflow-auto flexbox-col dc__gap-16">
                 {/* Info section */}
                 <div className="dc__icon-text-layout">
-                    <ICCalendar className="fcn-7 icon-dim-20 dc__no-shrink" />
+                    <ICCalendar className={`scn-7 ${BASE_ICON_CLASS}`} />
                     <time className="cn-9 fs-12 cn-9 fw-4 lh-20">
                         {executionInfo?.triggeredOn ? getFormattedTriggerTime(executionInfo.triggeredOn) : '--'}
                     </time>
 
-                    <ICUserCircle className="fcn-7 icon-dim-20 dc__no-shrink" />
+                    <ICPerson className={`fcn-7 ${BASE_ICON_CLASS}`} />
                     <Tooltip content={triggeredByEmail}>
                         <span className="cn-9 fs-12 fw-4 lh-20 dc__truncate">{triggeredByEmail}</span>
                     </Tooltip>
