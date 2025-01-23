b052f3e20683b3689150edad2431f00019efdfe6
Eshank Vaish <48060426+eshankvaish@users.noreply.github.com>
2025-01-23 13:29:51 +0530

    fix: accessibility issue with label

--- src/Shared/Components/FormFieldWrapper/FormFieldLabel.tsx
+++ src/Shared/Components/FormFieldWrapper/FormFieldLabel.tsx
@@ -1,12 +1,15 @@
 import { FormFieldLabelProps } from './types'
+import { getFormLabelElementId } from './utils'
 
 const FormFieldLabel = ({ label, inputId, required }: FormFieldLabelProps) => {
     if (!label) {
         return null
     }
 
+    const labelId = getFormLabelElementId(inputId)
+
     return (
-        <label className="fs-13 lh-20 cn-7 fw-4 dc__block mb-0" htmlFor={inputId} data-testid={`label-${inputId}`}>
+        <label className="fs-13 lh-20 cn-7 fw-4 dc__block mb-0" htmlFor={inputId} id={labelId} data-testid={labelId}>
             {typeof label === 'string' ? (
                 <span className={`flex left ${required ? 'dc__required-field' : ''}`}>
                     <span className="dc__truncate">{label}</span>
