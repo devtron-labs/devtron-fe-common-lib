efef114588ff2dd51f64dbdce2f6b9ba00af7aa9
Amrit Kashyap Borah <amrit.borah@devtron.ai>
2025-01-24 17:09:00 +0530

    refactor: migrate DRAIN_NODE_MODAL_MESSAGING from dashboard

--- src/Pages/ResourceBrowser/constants.ts
+++ src/Pages/ResourceBrowser/constants.ts
@@ -4,3 +4,35 @@ export const ALL_NAMESPACE_OPTION: Readonly<Pick<SelectPickerOptionType<string>,
     value: 'all',
     label: 'All namespaces',
 }
+
+export const DRAIN_NODE_MODAL_MESSAGING = {
+    GracePeriod: {
+        heading: 'Grace period',
+        infoText:
+            'Period of time in seconds given to each pod to terminate gracefully. If negative, the default value specified in the pod will be used.',
+    },
+    DeleteEmptyDirectoryData: {
+        heading: 'Delete empty directory data',
+        infoText: 'Enabling this field will delete the pods using empty directory data when the node is drained.',
+    },
+    DisableEviction: {
+        heading: 'Disable eviction (use with caution)',
+        infoText: `Enabling this field will force drain to use delete, even if eviction is supported. This will bypass checking PodDisruptionBudgets.
+            Note: Make sure to use with caution.`,
+    },
+    ForceDrain: {
+        heading: 'Force drain',
+        infoText:
+            'Enabling this field will force drain a node even if there are pods that do not declare a controller.',
+    },
+    IgnoreDaemonSets: {
+        heading: 'Ignore DaemonSets',
+        infoText: 'Enabling this field will ignore DaemonSet-managed pods.',
+    },
+    Actions: {
+        infoText: 'Drain will cordon off the node and evict all pods of the node.',
+        drain: 'Drain node',
+        draining: 'Draining node',
+        cancel: 'Cancel',
+    },
+}
