25e00abb4a56ed0f4d843aedb5e725df39c68899
Eshank Vaish <48060426+eshankvaish@users.noreply.github.com>
2025-01-31 12:43:51 +0530

    fix: add missing ref

--- src/Shared/Components/CustomInput/CustomInput.tsx
+++ src/Shared/Components/CustomInput/CustomInput.tsx
@@ -120,6 +120,7 @@ const CustomInput = ({
                     onBlur={handleBlur}
                     onKeyDown={handleKeyDown}
                     type={type}
+                    ref={inputRef}
                     className={`${COMPONENT_SIZE_TYPE_TO_FONT_AND_BLOCK_PADDING_MAP[size]} ${COMPONENT_SIZE_TYPE_TO_INLINE_PADDING_MAP[size]} ${getFormFieldBorderClassName(borderRadiusConfig)} ${endIconButtonConfig ? `custom-input__with-icon-button--${size}` : ''} w-100 dc__overflow-auto`}
                 />
                 {endIconButtonConfig && (
