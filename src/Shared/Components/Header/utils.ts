7ebcd186996881eb0625cfb786ebc2b63d0d1692
shivani170 <shivani@devtron.ai>
2025-02-05 12:51:33 +0530

    fix/posthog-event

--- src/Shared/Components/Header/utils.ts
+++ src/Shared/Components/Header/utils.ts
@@ -20,9 +20,9 @@ import { LOGIN_COUNT } from '../../../Common'
 const millisecondsInDay = 86400000
 export const getDateInMilliseconds = (days) => 1 + new Date().valueOf() + (days ?? 0) * millisecondsInDay
 
-export const handlePostHogEventUpdate = async (e, eventName?: string): Promise<void> => {
+export const handlePostHogEventUpdate = async (eventName?: string): Promise<void> => {
     const payload = {
-        eventType: eventName || e.target?.dataset.posthog,
+        eventType: eventName,
         key: LOGIN_COUNT,
         value: '',
         active: true,
