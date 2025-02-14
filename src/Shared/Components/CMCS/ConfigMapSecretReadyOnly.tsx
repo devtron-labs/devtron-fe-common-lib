96ce83e7acc41ea6f337df4c726556be37248a16
vivek-devtron <vivek@devtron.ai>
2025-02-14 13:40:44 +0530

    removed commented code

--- src/Shared/Components/CMCS/ConfigMapSecretReadyOnly.tsx
+++ src/Shared/Components/CMCS/ConfigMapSecretReadyOnly.tsx
@@ -66,16 +66,6 @@ const ConfigMapSecretReadyOnly = ({
                     ) : null,
                 )}
             </div>
-            {/* {!hideCodeEditor && displayValues.data && (
-                <CodeEditor.Container>
-                    <CodeEditor value={displayValues.data} mode={MODES.YAML} height="auto" readOnly>
-                        <CodeEditor.Header className="flex dc__content-space px-16 py-6 dc__border-bottom">
-                            <p className="m-0 fs-13 lh-20 fw-6 cn-9">Data</p>
-                            <CodeEditor.Clipboard />
-                        </CodeEditor.Header>
-                    </CodeEditor>
-                </CodeEditor.Container>
-            )} */}
             {!hideCodeEditor && displayValues.data && (
                 <div className="dc__border br-4">
                     <div className="px-16 py-6 dc__border-bottom flex dc__content-space">
