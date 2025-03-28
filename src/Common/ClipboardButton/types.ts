9a22d8a0cfa1d20f83832351fef2e556925abcc5
Arun Jain <arun.jain@devtron.ai>
2025-03-28 15:36:35 +0530

    chore: add jsdoc comment in clipboard button

--- src/Common/ClipboardButton/types.ts
+++ src/Common/ClipboardButton/types.ts
@@ -16,8 +16,17 @@
 
 export default interface ClipboardProps {
     content: string
+    /**
+     * tippy text before copying
+     */
     initialTippyText?: string
+    /**
+     * tippy text after copying
+     */
     copiedTippyText?: string
+    /**
+     * reset duration after copying
+     */
     duration?: number
     copyToClipboardPromise?: Promise<void>
     rootClassName?: string
