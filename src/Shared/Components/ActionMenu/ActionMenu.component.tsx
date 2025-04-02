2408c4aefa7a8a3560eaab856f40eadc54788fcf
Eshank Vaish <48060426+eshankvaish@users.noreply.github.com>
2025-04-02 15:55:38 +0530

    feat: add action menu options

--- src/Shared/Components/ActionMenu/ActionMenu.component.tsx
+++ src/Shared/Components/ActionMenu/ActionMenu.component.tsx
@@ -1,9 +1,62 @@
-import { ActionMenuProps } from './types'
+import PopupMenu from '@Common/PopupMenu'
+import { Tooltip } from '@Common/Tooltip'
+import { ActionMenuOptionProps, ActionMenuProps } from './types'
+import { getTooltipProps } from '../SelectPicker/common'
 
-const ActionMenu = (props: ActionMenuProps) => {
-    const { children } = props
+const ActionMenuOption = ({ option, onClick, disableDescriptionEllipsis }: ActionMenuOptionProps) => {
+    const iconBaseClass = 'dc__no-shrink icon-dim-16 flex dc__fill-available-space'
+    const { description, label, startIcon, endIcon, tooltipProps } = option
 
-    return <div>{children}</div>
+    const handleClick = () => {
+        onClick(option)
+    }
+
+    return (
+        <Tooltip {...getTooltipProps(tooltipProps)}>
+            <div
+                className={`flex left dc__gap-8 ${description ? 'top' : ''} py-6 px-8 cursor dc__hover-n50`}
+                onClick={handleClick}
+            >
+                {startIcon && <div className={`${iconBaseClass} mt-2`}>{startIcon}</div>}
+                <div className="flex-grow-1">
+                    <Tooltip content={label} placement="right">
+                        <h4 className="m-0 fs-13 cn-9 fw-4 lh-20 dc__truncate">{label}</h4>
+                    </Tooltip>
+                    {description &&
+                        (typeof description === 'string' ? (
+                            <p
+                                className={`m-0 fs-12 fw-4 lh-18 cn-7 ${!disableDescriptionEllipsis ? 'dc__ellipsis-right__2nd-line' : 'dc__word-break'}`}
+                            >
+                                {description}
+                            </p>
+                        ) : (
+                            <div className="fs-12 lh-18">{description}</div>
+                        ))}
+                </div>
+                {endIcon && <div className={iconBaseClass}>{endIcon}</div>}
+            </div>
+        </Tooltip>
+    )
 }
+
+const ActionMenu = ({ options, disableDescriptionEllipsis, children, onClick }: ActionMenuProps) => (
+    <PopupMenu autoClose>
+        <PopupMenu.Button isKebab rootClassName="flex left dc__no-background">
+            {children}
+        </PopupMenu.Button>
+        <PopupMenu.Body rootClassName="dc__border py-4">
+            {options.length > 0
+                ? options.map((option) => (
+                      <ActionMenuOption
+                          key={option.value}
+                          option={option}
+                          onClick={onClick}
+                          disableDescriptionEllipsis={disableDescriptionEllipsis}
+                      />
+                  ))
+                : 'No Options'}
+        </PopupMenu.Body>
+    </PopupMenu>
+)
 
 export default ActionMenu
--- src/Shared/Components/ActionMenu/types.ts
+++ src/Shared/Components/ActionMenu/types.ts
@@ -1,8 +1,13 @@
 import { ReactElement } from 'react'
-import { SelectPickerOptionType } from '../SelectPicker'
+import { GroupBase, OptionsOrGroups } from 'react-select'
+import { SelectPickerOptionType, SelectPickerProps } from '../SelectPicker'
 
-export interface ActionMenuProps {
+export interface ActionMenuProps extends Pick<SelectPickerProps, 'disableDescriptionEllipsis'> {
     children: ReactElement
-    options: SelectPickerOptionType[]
+    options: OptionsOrGroups<SelectPickerOptionType, GroupBase<SelectPickerOptionType>>
     onClick: (option: SelectPickerOptionType) => void
+}
+
+export interface ActionMenuOptionProps extends Pick<ActionMenuProps, 'onClick' | 'disableDescriptionEllipsis'> {
+    option: SelectPickerOptionType
 }
