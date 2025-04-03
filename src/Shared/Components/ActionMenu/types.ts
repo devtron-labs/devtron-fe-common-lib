ef92b1ae575cb6b26861a3e6007df258c33b0fa1
Eshank Vaish <48060426+eshankvaish@users.noreply.github.com>
2025-04-03 12:46:56 +0530

    feat: add disabled and option type

--- src/Shared/Components/ActionMenu/types.ts
+++ src/Shared/Components/ActionMenu/types.ts
@@ -2,12 +2,20 @@ import { ReactElement } from 'react'
 import { GroupBase, OptionsOrGroups } from 'react-select'
 import { SelectPickerOptionType, SelectPickerProps } from '../SelectPicker'
 
+type ActionMenuOptionType = SelectPickerOptionType & {
+    isDisabled?: boolean
+    /**
+     * @default 'neutral'
+     */
+    type?: 'neutral' | 'negative'
+}
+
 export interface ActionMenuProps extends Pick<SelectPickerProps, 'disableDescriptionEllipsis'> {
     children: ReactElement
-    options: OptionsOrGroups<SelectPickerOptionType, GroupBase<SelectPickerOptionType>>
+    options: OptionsOrGroups<ActionMenuOptionType, GroupBase<ActionMenuOptionType>>
     onClick: (option: SelectPickerOptionType) => void
 }
 
 export interface ActionMenuOptionProps extends Pick<ActionMenuProps, 'onClick' | 'disableDescriptionEllipsis'> {
-    option: SelectPickerOptionType
+    option: ActionMenuOptionType
 }
