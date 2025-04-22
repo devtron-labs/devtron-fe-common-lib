21d177c4bfe1b3788647d01569f51882c9d126f6
Amrit Kashyap Borah <amrit.borah@devtron.ai>
2025-04-22 12:58:24 +0530

    fix: update NewClusterFormProps type

--- src/Pages/GlobalConfigurations/ClustersAndEnvironments/types.ts
+++ src/Pages/GlobalConfigurations/ClustersAndEnvironments/types.ts
@@ -1,11 +1,8 @@
-import { Dispatch, SetStateAction } from 'react'
-
 export interface NewClusterFormFooterProps {
     apiCallInProgress: boolean
     handleModalClose: () => void
 }
 
-export interface NewClusterFormProps extends NewClusterFormFooterProps {
-    setApiCallInProgress: Dispatch<SetStateAction<boolean>>
+export interface NewClusterFormProps extends Pick<NewClusterFormFooterProps, 'handleModalClose'> {
     FooterComponent: React.FunctionComponent<NewClusterFormFooterProps>
 }
