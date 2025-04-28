6fe547f9dc7989038995fd73c4d029870fe04329
Amrit Kashyap Borah <amrit.borah@devtron.ai>
2025-04-28 11:32:21 +0530

    fix: review comment in testimonials

--- src/Shared/Components/LoginBanner/Testimonials.components.tsx
+++ src/Shared/Components/LoginBanner/Testimonials.components.tsx
@@ -3,11 +3,11 @@ import { TestimonialCardConfig } from './types'
 
 const TestimonialContent = ({ quote, name, designation, iconName }: TestimonialCardConfig) => (
     <>
-        <div className="fs-14 fw-4 lh-1-5 cn-9 dc__truncate--clamp-4">{quote}&quot;</div>
+        <p className="fs-14 fw-4 lh-1-5 cn-9 dc__truncate--clamp-4 m-0">{quote}&quot;</p>
         <div className="flex dc__content-space">
             <div>
-                <div className="fs-13 fw-6 lh-1-5 cn-9">{name}</div>
-                <div className="fs-12 fw-4 lh-1-5 cn-7 dc__truncate">{designation}</div>
+                <span className="fs-13 fw-6 lh-1-5 cn-9">{name}</span>
+                <span className="fs-12 fw-4 lh-1-5 cn-7 dc__truncate">{designation}</span>
             </div>
             {iconName && (
                 <div className="dc__fill-available-space w-auto-imp h-36">
