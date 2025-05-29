37bcb8865fa831420b632cd0891a826b1b748278
Rohit Raj <rohit.raj@devtron.ai>
2025-05-29 13:43:22 +0530

    fix: Constants - correct DOCUMENTATION_VERSION path

--- src/Common/Constants.ts
+++ src/Common/Constants.ts
@@ -19,7 +19,7 @@ export const Host = window?.__ORCHESTRATOR_ROOT__ ?? '/orchestrator'
 
 export const DOCUMENTATION_HOME_PAGE = 'https://docs.devtron.ai'
 export const DEVTRON_HOME_PAGE = 'https://devtron.ai/'
-export const DOCUMENTATION_VERSION = '/v/v0.7'
+export const DOCUMENTATION_VERSION = '/devtron/v0.7'
 export const DISCORD_LINK = 'https://discord.devtron.ai/'
 export const DEFAULT_JSON_SCHEMA_URI = 'https://json-schema.org/draft/2020-12/schema'
 
