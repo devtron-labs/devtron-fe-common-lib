a0ea78ee0d10564578e05688cbb6e71948edc37f
Amrit Kashyap Borah <amrit.borah@devtron.ai>
2025-07-18 00:19:57 +0530

    fix: bulk edit show envName

---
+++ src/Pages/BulkEdit/types.ts
@@ -0,0 +1,57 @@
+export interface BulkEditConfigV2Type {
+    gvk: string
+    readme: string
+    schema: Record<string, any>
+}
+
+export interface AppEnvDetail {
+    appName: string
+    envName?: string
+    message: string
+}
+
+export interface ObjectsWithAppEnvDetail {
+    appName: string
+    envName?: string
+    message: string
+    names: string[]
+}
+
+export interface DeploymentTemplateResponse {
+    message: string[]
+    failure: AppEnvDetail[]
+    successful: AppEnvDetail[]
+}
+
+export interface CmCsResponse {
+    message: string[]
+    failure: ObjectsWithAppEnvDetail[]
+    successful: ObjectsWithAppEnvDetail[]
+}
+
+export interface BulkEditResponseType {
+    deploymentTemplate?: DeploymentTemplateResponse
+    configMap?: CmCsResponse
+    secret?: CmCsResponse
+}
+
+export interface ImpactedDeploymentTemplate {
+    appId?: number
+    envId?: number
+    appName: string
+    envName?: string
+}
+
+export interface ImpactedCmCs {
+    appId?: number
+    envId?: number
+    appName: string
+    envName?: string
+    names: string[]
+}
+
+export interface ImpactedObjectsType {
+    deploymentTemplate?: ImpactedDeploymentTemplate[]
+    configMap?: ImpactedCmCs[]
+    secret?: ImpactedCmCs[]
+}
