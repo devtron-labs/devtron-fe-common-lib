04952f111caedba11b1e4b5ac25a2b96640ac927
Amrit Kashyap Borah <amrit.borah@devtron.ai>
2025-08-28 15:02:01 +0530

    refactor: move cost visibility stuff to fe-lib

--- src/Pages-Devtron-2.0/CostVisibility/Shared/types.ts
+++ src/Pages-Devtron-2.0/CostVisibility/Shared/types.ts
@@ -8,13 +8,3 @@ export enum CostBreakdownViewType {
 export enum CostBreakdownItemViewParamsType {
     ITEM_NAME = 'itemName',
 }
-
-type TrendSignType = '+' | '-'
-export type CostTrendType = '0' | `${TrendSignType}${number}`
-
-export enum CostBreakdownSubComponents {
-    MEMORY = 'memory',
-    CPU = 'cpu',
-    STORAGE = 'storage',
-    GPU = 'gpu',
-}
