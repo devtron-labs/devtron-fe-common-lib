d49cb158096b0f25ac194d7658bb3f5df3260927
Amrit Kashyap Borah <amrit.borah@devtron.ai>
2025-09-01 22:39:58 +0530

    feat: segmented bar charts xs size

--- src/Common/SegmentedBarChart/utils.ts
+++ src/Common/SegmentedBarChart/utils.ts
@@ -4,6 +4,9 @@ export const getBarHeightForSize = (size: ComponentSizeType) => {
     switch (size) {
         case ComponentSizeType.large:
             return 'h-20'
+        case ComponentSizeType.xs:
+            return 'h-6'
+        case ComponentSizeType.small:
         default:
             return 'h-8'
     }
