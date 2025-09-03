9256118e949047990ce651b4c49c09c8206e543a
AbhishekA1509 <abhishek@devtron.ai>
2025-09-03 11:02:45 +0530

    feat: add new icons for ArgoCD and FluxCD applications
    - Added ic-argocd-app.svg and ic-fluxcd-app.svg icons to the project.
    - Updated Icon.tsx to include imports for the new icons.
    - Added ic-symbol-greater-than.svg icon to the project.
    - Updated KeyboardShortcut component to utilize the new icon for the '>' keyboard shortcut.
    - Introduced constants.tsx to map keyboard keys to UI representations, including icons.
    - Extended UserPreference types to accommodate new command bar items for app management.

--- src/Shared/Components/Icon/Icon.tsx
+++ src/Shared/Components/Icon/Icon.tsx
@@ -7,6 +7,7 @@ import { ReactComponent as ICAdd } from '@IconsV2/ic-add.svg'
 import { ReactComponent as ICAmazonEks } from '@IconsV2/ic-amazon-eks.svg'
 import { ReactComponent as ICAppGroup } from '@IconsV2/ic-app-group.svg'
 import { ReactComponent as ICAppTemplate } from '@IconsV2/ic-app-template.svg'
+import { ReactComponent as ICArgocdApp } from '@IconsV2/ic-argocd-app.svg'
 import { ReactComponent as ICArrowClockwise } from '@IconsV2/ic-arrow-clockwise.svg'
 import { ReactComponent as ICArrowRight } from '@IconsV2/ic-arrow-right.svg'
 import { ReactComponent as ICArrowSquareOut } from '@IconsV2/ic-arrow-square-out.svg'
@@ -103,6 +104,7 @@ import { ReactComponent as ICFilesChanged } from '@IconsV2/ic-files-changed.svg'
 import { ReactComponent as ICFilter } from '@IconsV2/ic-filter.svg'
 import { ReactComponent as ICFilterApplied } from '@IconsV2/ic-filter-applied.svg'
 import { ReactComponent as ICFlask } from '@IconsV2/ic-flask.svg'
+import { ReactComponent as ICFluxcdApp } from '@IconsV2/ic-fluxcd-app.svg'
 import { ReactComponent as ICFolder } from '@IconsV2/ic-folder.svg'
 import { ReactComponent as ICFolderColor } from '@IconsV2/ic-folder-color.svg'
 import { ReactComponent as ICFolderUser } from '@IconsV2/ic-folder-user.svg'
@@ -218,6 +220,7 @@ import { ReactComponent as ICStrategyRollingColor } from '@IconsV2/ic-strategy-r
 import { ReactComponent as ICSuccess } from '@IconsV2/ic-success.svg'
 import { ReactComponent as ICSun } from '@IconsV2/ic-sun.svg'
 import { ReactComponent as ICSuspended } from '@IconsV2/ic-suspended.svg'
+import { ReactComponent as ICSymbolGreaterThan } from '@IconsV2/ic-symbol-greater-than.svg'
 import { ReactComponent as ICTag } from '@IconsV2/ic-tag.svg'
 import { ReactComponent as ICTata1mg } from '@IconsV2/ic-tata1mg.svg'
 import { ReactComponent as ICTerminal } from '@IconsV2/ic-terminal.svg'
@@ -258,6 +261,7 @@ export const iconMap = {
     'ic-amazon-eks': ICAmazonEks,
     'ic-app-group': ICAppGroup,
     'ic-app-template': ICAppTemplate,
+    'ic-argocd-app': ICArgocdApp,
     'ic-arrow-clockwise': ICArrowClockwise,
     'ic-arrow-right': ICArrowRight,
     'ic-arrow-square-out': ICArrowSquareOut,
@@ -354,6 +358,7 @@ export const iconMap = {
     'ic-filter-applied': ICFilterApplied,
     'ic-filter': ICFilter,
     'ic-flask': ICFlask,
+    'ic-fluxcd-app': ICFluxcdApp,
     'ic-folder-color': ICFolderColor,
     'ic-folder-user': ICFolderUser,
     'ic-folder': ICFolder,
@@ -469,6 +474,7 @@ export const iconMap = {
     'ic-success': ICSuccess,
     'ic-sun': ICSun,
     'ic-suspended': ICSuspended,
+    'ic-symbol-greater-than': ICSymbolGreaterThan,
     'ic-tag': ICTag,
     'ic-tata1mg': ICTata1mg,
     'ic-terminal-fill': ICTerminalFill,
