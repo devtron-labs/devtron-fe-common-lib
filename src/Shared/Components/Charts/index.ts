84d417c8cf032c574b72c58d1e970a475bb02d26
Arun Jain <arun.jain@devtron.ai>
2025-09-10 11:46:45 +0530

    chore: add export for plugins

--- src/Shared/Components/Charts/index.ts
+++ src/Shared/Components/Charts/index.ts
@@ -1,3 +1,4 @@
 export { default as Chart } from './Chart.component'
 export { CHART_COLORS } from './constants'
+export * from './plugins'
 export type { ChartColorKey, ChartProps, ChartType, SimpleDataset, SimpleDatasetForPie } from './types'
