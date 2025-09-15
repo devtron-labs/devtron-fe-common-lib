32a121f33a551ce2261354bf82b0b61e30ba8072
Arun Jain <arun.jain@devtron.ai>
2025-09-15 22:14:14 +0530

    feat: add icons for license dashboard login page

--- src/Shared/Components/Icon/Icon.tsx
+++ src/Shared/Components/Icon/Icon.tsx
@@ -98,9 +98,11 @@ import { ReactComponent as ICFlask } from '@IconsV2/ic-flask.svg'
 import { ReactComponent as ICFolder } from '@IconsV2/ic-folder.svg'
 import { ReactComponent as ICFolderColor } from '@IconsV2/ic-folder-color.svg'
 import { ReactComponent as ICFolderUser } from '@IconsV2/ic-folder-user.svg'
+import { ReactComponent as ICFreemiumDevtron } from '@IconsV2/ic-freemium-devtron.svg'
 import { ReactComponent as ICGavel } from '@IconsV2/ic-gavel.svg'
 import { ReactComponent as ICGear } from '@IconsV2/ic-gear.svg'
 import { ReactComponent as ICGift } from '@IconsV2/ic-gift.svg'
+import { ReactComponent as ICGiftFilled } from '@IconsV2/ic-gift-filled.svg'
 import { ReactComponent as ICGiftGradient } from '@IconsV2/ic-gift-gradient.svg'
 import { ReactComponent as ICGit } from '@IconsV2/ic-git.svg'
 import { ReactComponent as ICGitBranch } from '@IconsV2/ic-git-branch.svg'
@@ -177,6 +179,7 @@ import { ReactComponent as ICPencil } from '@IconsV2/ic-pencil.svg'
 import { ReactComponent as ICPlayOutline } from '@IconsV2/ic-play-outline.svg'
 import { ReactComponent as ICQuay } from '@IconsV2/ic-quay.svg'
 import { ReactComponent as ICQuote } from '@IconsV2/ic-quote.svg'
+import { ReactComponent as ICRatings } from '@IconsV2/ic-ratings.svg'
 import { ReactComponent as ICResizeHandle } from '@IconsV2/ic-resize-handle.svg'
 import { ReactComponent as ICRocketGear } from '@IconsV2/ic-rocket-gear.svg'
 import { ReactComponent as ICRocketLaunch } from '@IconsV2/ic-rocket-launch.svg'
@@ -205,6 +208,7 @@ import { ReactComponent as ICStrategyRecreateColor } from '@IconsV2/ic-strategy-
 import { ReactComponent as ICStrategyRolling } from '@IconsV2/ic-strategy-rolling.svg'
 import { ReactComponent as ICStrategyRollingColor } from '@IconsV2/ic-strategy-rolling-color.svg'
 import { ReactComponent as ICSuccess } from '@IconsV2/ic-success.svg'
+import { ReactComponent as ICSuccessBlue } from '@IconsV2/ic-success-blue.svg'
 import { ReactComponent as ICSun } from '@IconsV2/ic-sun.svg'
 import { ReactComponent as ICSuspended } from '@IconsV2/ic-suspended.svg'
 import { ReactComponent as ICTag } from '@IconsV2/ic-tag.svg'
@@ -337,8 +341,10 @@ export const iconMap = {
     'ic-folder-color': ICFolderColor,
     'ic-folder-user': ICFolderUser,
     'ic-folder': ICFolder,
+    'ic-freemium-devtron': ICFreemiumDevtron,
     'ic-gavel': ICGavel,
     'ic-gear': ICGear,
+    'ic-gift-filled': ICGiftFilled,
     'ic-gift-gradient': ICGiftGradient,
     'ic-gift': ICGift,
     'ic-git-branch': ICGitBranch,
@@ -416,6 +422,7 @@ export const iconMap = {
     'ic-play-outline': ICPlayOutline,
     'ic-quay': ICQuay,
     'ic-quote': ICQuote,
+    'ic-ratings': ICRatings,
     'ic-resize-handle': ICResizeHandle,
     'ic-rocket-gear': ICRocketGear,
     'ic-rocket-launch': ICRocketLaunch,
@@ -443,6 +450,7 @@ export const iconMap = {
     'ic-strategy-recreate': ICStrategyRecreate,
     'ic-strategy-rolling-color': ICStrategyRollingColor,
     'ic-strategy-rolling': ICStrategyRolling,
+    'ic-success-blue': ICSuccessBlue,
     'ic-success': ICSuccess,
     'ic-sun': ICSun,
     'ic-suspended': ICSuspended,
