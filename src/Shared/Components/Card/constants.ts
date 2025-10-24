5d05befb760cb942829d123dce02cfa891602683
Amrit Kashyap Borah <amrit.borah@devtron.ai>
2025-10-24 14:32:41 +0530

    fix: review comments

--- src/Shared/Components/Card/constants.ts
+++ src/Shared/Components/Card/constants.ts
@@ -1,8 +1,10 @@
+import { CardProps } from './types'
+
 export const DEFAULT_BORDER_RADIUS = 8
 
 export const DEFAULT_FLEX_GAP = 0
 
-export const VARIANT_TO_BORDER_CLASS_MAP = {
+export const VARIANT_TO_BORDER_CLASS_MAP: Record<CardProps['variant'], string> = {
     'primary--outlined': 'border__primary',
     'secondary--outlined': 'border__secondary',
 }
