4264851ee3a4583e51df235fff474411d34448d8
shivani170 <shivani@devtron.ai>
2025-11-07 14:02:54 +0530

    feat: breadcrumb css updated

--- src/Common/BreadCrumb/BreadcrumbStore.tsx
+++ src/Common/BreadCrumb/BreadcrumbStore.tsx
@@ -22,8 +22,13 @@ const initialState = {
 }
 
 export const BreadcrumbText = ({ heading, isActive, shouldTruncate = false }: BreadcrumbTextProps) => (
-    <h2 className={`m-0 fs-16 fw-6 lh-32 ${shouldTruncate ? 'dc__truncate' : ''} ${isActive ? 'cn-9' : 'cb-5'}`}>{heading}</h2>
+    <span className={`dc__breadcrumb-text cb-5 fs-16 lh-1-5 ${shouldTruncate ? 'dc__truncate' : ''} ${isActive ? 'cn-9 fw-6' : 'cb-5 fw-4 dc__mxw-155 dc__ellipsis-right'}`}>{heading}</span>
 )
+
+export const getBreadCrumbSeparator = (sep: string = '/') => (
+    <span className="dc__devtron-breadcrumb__item__separator">{sep}</span>
+)
+
 
 const Store = ({ children }) => {
     const [state, setState] = useState(initialState)
