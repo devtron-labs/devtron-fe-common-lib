c230c059e6f0aa172bc1b825e8197845c2b70756
shivani170 <shivani@devtron.ai>
2026-01-30 13:32:15 +0530

    chore: icon update for configuration

--- src/Shared/Components/Icon/Icon.tsx
+++ src/Shared/Components/Icon/Icon.tsx
@@ -89,6 +89,7 @@ import { ReactComponent as ICClusterIsolated } from '@IconsV2/ic-cluster-isolate
 import { ReactComponent as ICCode } from '@IconsV2/ic-code.svg'
 import { ReactComponent as ICCoins } from '@IconsV2/ic-coins.svg'
 import { ReactComponent as ICCoinsColorAnimated } from '@IconsV2/ic-coins-color-animated.svg'
+import { ReactComponent as ICConfigurationFile } from '@IconsV2/ic-configuration-file.svg'
 import { ReactComponent as ICContainer } from '@IconsV2/ic-container.svg'
 import { ReactComponent as ICContainerRegistry } from '@IconsV2/ic-container-registry.svg'
 import { ReactComponent as ICCookr } from '@IconsV2/ic-cookr.svg'
@@ -411,6 +412,7 @@ export const iconMap = {
     'ic-code': ICCode,
     'ic-coins-color-animated': ICCoinsColorAnimated,
     'ic-coins': ICCoins,
+    'ic-configuration-file': ICConfigurationFile,
     'ic-container-registry': ICContainerRegistry,
     'ic-container': ICContainer,
     'ic-cookr': ICCookr,
