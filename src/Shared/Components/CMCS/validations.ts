3efda76d37b4c350db7c87c76c66c7494df863f0
Arun Jain <arun.jain@devtron.ai>
2026-09-17 13:02:47 +0530

    feat: enhance validation message for config map and secret names to include scope variable format

--- src/Shared/Components/CMCS/validations.ts
+++ src/Shared/Components/CMCS/validations.ts
@@ -239,7 +239,7 @@ export const getConfigMapSecretFormValidations: UseFormValidations<ConfigMapSecr
             pattern: {
                 value: PATTERNS.CONFIGMAP_AND_SECRET_NAME_WITH_SCOPE_VARIABLES,
                 message:
-                    "Name must start and end with an alphanumeric character. It can contain only lowercase alphanumeric characters, '-' or '.'",
+                    "Name must start and end with an alphanumeric character. It can contain only lowercase alphanumeric characters, '-' or '.'. Alternatively, a scope variable can be used in the format @{{variable-name}}",
             },
             custom: {
                 isValid: (value) => value.length <= 253,
